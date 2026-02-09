ead9599330344fe9d775b538a05208127f0a6b14
Adam <kuniaka@natur.cuni.cz>
2026-02-09 16:16:09 +0100

    Updated the name of the dataset

--- train.py
+++ train.py
@@ -1,75 +1,15 @@
 from torch.utils.data import DataLoader
-from transformers import EsmModel, AutoTokenizer
+from transformers import AutoTokenizer
 import torch
 import torch.nn as nn
 import wandb
 import torchmetrics
 from torchmetrics import MetricCollection, MatthewsCorrCoef, JaccardIndex, F1Score
 from torchmetrics.classification import MultilabelExactMatch
-from data_utils import ProteinLocalizationDataset
+from data_utils import SwissProtDataset
 from focal_loss import MultiLabelFocalLoss
+from model import ProteinLocalizator
 import numpy as np
-
-class AttentionPooling(nn.Module):
-    def __init__(self, hidden_size):
-        super().__init__()
-        self.learnable_vector = nn.Parameter(torch.empty(1, hidden_size))
-        self.learnable_vector = nn.init.xavier_uniform_(self.learnable_vector)
-
-        # 1D Gaussian filter
-        kernel_size = 5
-        std = 1
-        x = torch.arange(kernel_size) - (kernel_size - 1) // 2  # array [-2, -1, 0, 1, 2]
-        g_filter = torch.exp(-(x.pow(2)) / (2 * std ** 2))  # Gaussian filter
-        g_filter = g_filter.view(1, 1, -1)  # conv1d expects dimensions [in_channels, out_channels, width]
-        self.register_buffer("gaussian_filter", g_filter)
-
-    def forward(self, embeddings: torch.Tensor, mask: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        assert embeddings.shape[2] == self.learnable_vector.T.shape[0], f"Dimension mismatch: {embeddings.shape[2]} != {self.learnable_vector.T.shape[0]}"
-        raw_scores = embeddings @ self.learnable_vector.T.squeeze(-1)
-
-        smoothed_scores = nn.functional.conv1d(raw_scores.unsqueeze(1), self.gaussian_filter, padding=2)
-        smoothed_scores = smoothed_scores.squeeze(1)  # [batch, seq_len]
-        smoothed_scores = smoothed_scores.masked_fill(mask == 0, value=-1e9)
-
-        # Softmax to get attention weights
-        attention_weights = nn.functional.softmax(smoothed_scores, dim=-1)
-        assert attention_weights.shape[1] == embeddings.shape[1], f"Dimension mismatch: {attention_weights.shape[1]} != {embeddings.shape[1]}"
-        # The weighted sum
-        final_representation = torch.bmm(attention_weights.unsqueeze(1), embeddings)
-
-        return final_representation, attention_weights
-
-
-class ProteinLocalizator(nn.Module):
-    def __init__(self, model_name="facebook/esm2_t33_650M_UR50D", num_labels=10):
-        super().__init__()
-        # Pretrained backbone, ESM-2 model
-        self.backbone = EsmModel.from_pretrained(model_name)
-        self.embedding_size = self.backbone.config.hidden_size
-        self.hidden_size = 128
-        self.dropout_prob = 0.1
-
-        self.attention_pooling = AttentionPooling(self.embedding_size)
-
-        self.classifier_head = nn.Sequential(
-            nn.Linear(in_features=self.embedding_size, out_features=self.hidden_size),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=self.dropout_prob),
-            nn.Linear(in_features=self.hidden_size, out_features=num_labels)
-        )
-
-    def forward(self, x: torch.Tensor, attention_mask: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        # Frozen backbone
-        with torch.no_grad():
-            outputs = self.backbone(input_ids=x, attention_mask=attention_mask)
-
-            embeddings = outputs.last_hidden_state # Shape: [Batch, Seq, 1280]
-        # Learnable parts
-        pooled_embedding, att_weights = self.attention_pooling(embeddings, attention_mask)
-        logits = self.classifier_head(pooled_embedding)
-
-        return logits, att_weights
 
 def train_one_epoch(model: nn.Module, criterion: nn.Module, optimizer: torch.optim, train_loader: DataLoader, train_metrics: MetricCollection, device: torch.device):
     model.train()
@@ -214,8 +154,8 @@ def test_all_splits(tokenizer, metrics, device, batch_size=64, warmup_epochs=1, 
         dev_folds = [p]
 
         # Create datasets
-        train_dataset = ProteinLocalizationDataset(tokenizer, train_folds)
-        dev_dataset = ProteinLocalizationDataset(tokenizer, dev_folds)
+        train_dataset = SwissProtDataset(tokenizer, train_folds)
+        dev_dataset = SwissProtDataset(tokenizer, dev_folds)
 
         # Get the class weights by extracting inverse frequencies
         counts = train_dataset.labels.sum(axis=0)
