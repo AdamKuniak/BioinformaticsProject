858ba2503dd8095ea858073935ed8f6dd1cd0028
Adam <kuniaka@natur.cuni.cz>
2026-02-24 12:37:34 +0100

    Added class for the active site predictor

--- Active_site_prediction/model.py
+++ Active_site_prediction/model.py
@@ -2,6 +2,26 @@ from transformers import EsmModel
 import torch
 import torch.nn as nn
 
+class ActiveSitePredictor(torch.nn.Module):
+    def __init__(self, neck, model_name="facebook/esm2_t33_650M_UR50D"):
+        super().__init__()
+        self.backbone = EsmModel.from_pretrained(model_name)
+        self.neck = neck
+        self.classification_head = ClassificationHead(embedding_size=self.backbone.config.hidden_size)
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor):
+        # Frozen backbone
+        with torch.no_grad():
+            outputs = self.backbone(input_ids=x, attention_mask=mask)
+            embeddings = outputs.last_hidden_state
+            # embeddings = outputs.last_hidden_state[:, 1:-1, :]  # strip <cls> and <eos>
+
+        # Neck
+        neck_output = self.neck(embeddings, mask)
+
+        # Classification head
+        return self.classification_head(neck_output)
+
 
 class ClassificationHead(torch.nn.Module):
     """
