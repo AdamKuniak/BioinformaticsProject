078d50fab850162ea865fa576ad05091e2927e27
Adam <kuniaka@natur.cuni.cz>
2026-02-24 19:49:11 +0100

    fixed typos in filename

--- In_cell_localization/data_utils.py
+++ In_cell_localization/data_utils.py
@@ -9,7 +9,7 @@ class SwissProtDataset(torch.utils.data.Dataset):
     tokenizer: Tokenizer object to tokenize protein sequences
     partition: list of integers representing the partition for cross validation
     """
-    def __init__(self, tokenizer, partition=None, root="./data/deep_loc_2_0", max_length=1024):
+    def __init__(self, tokenizer, partition=None, root="./data/", max_length=1024):
         super().__init__()
         dataset = pd.read_csv(root + "/Swissprot_Train_Validation_dataset.csv", sep=",")
 
@@ -102,7 +102,7 @@ class HPADataset(torch.utils.data.Dataset):
         Class representing a test HPA dataset
         tokenizer: Tokenizer object to tokenize protein sequences
     """
-    def __init__(self, tokenizer, root="./data/deep_loc_2_0", max_length=1024):
+    def __init__(self, tokenizer, root="./data/", max_length=1024):
         super().__init__()
         self.tokenizer = tokenizer
         self.max_length = max_length
@@ -129,4 +129,49 @@ class HPADataset(torch.utils.data.Dataset):
             "input_ids": tokenized_seq["input_ids"].squeeze(0),
             "attention_mask": tokenized_seq["attention_mask"].squeeze(0),
             "label": label
+        }
+
+
+class PrecomputedHPADataset(torch.utils.data.Dataset):
+    """
+        Dataset that loads precomputed ESM-2 embeddings from memory-mapped files for the HPA test set.
+        Run precompute.py first to generate the required files.
+    """
+    def __init__(self, root="./data/test/hpa_precomputed_embeddings"):
+        super().__init__()
+        metadata = torch.load(os.path.join(root, "metadata.pt"), weights_only=False)
+        self._total = metadata["total"]
+        self._max_length = metadata["max_length"]
+        self._hidden_dim = metadata["hidden_dim"]
+        self._root = root
+
+        all_labels = metadata["labels"].numpy()
+        self.indices = np.arange(self._total)
+        self.labels = all_labels
+
+        self.LABEL_COLUMNS = ["Cell membrane", "Cytoplasm", "Endoplasmic reticulum", "Golgi apparatus", "Lysosome/Vacuole", "Mitochondrion", "Nucleus", "Peroxisome"]
+
+        self._embeddings = None
+        self.masks = None
+
+    def open_memmaps(self):
+        self._embeddings = np.memmap(
+            os.path.join(self._root, "embeddings.dat"),
+            dtype=np.float16, mode="r", shape=(self._total, self._max_length, self._hidden_dim)
+        )
+        self.masks = np.memmap(
+            os.path.join(self._root, "masks.dat"),
+            dtype=np.bool_, mode="r", shape=(self._total, self._max_length)
+        )
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx):
+        if self._embeddings is None:
+            self.open_memmaps()
+        return {
+            "embedding": torch.from_numpy(self._embeddings[idx].copy()),
+            "attention_mask": torch.from_numpy(self.masks[idx].copy()).long(),
+            "label": torch.tensor(self.labels[idx], dtype=torch.float),
         }
