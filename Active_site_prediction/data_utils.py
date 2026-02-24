812be27d2a4daba388a6abee84ca7bf500055dc7
Adam <kuniaka@natur.cuni.cz>
2026-02-24 22:08:26 +0100

    Added precomputed Uniprot dataset class

--- Active_site_prediction/data_utils.py
+++ Active_site_prediction/data_utils.py
@@ -1,5 +1,7 @@
 import torch
 import json
+import os
+import numpy as np
 
 
 class MCSADataset(torch.utils.data.Dataset):
@@ -69,8 +71,65 @@ class UniprotDataset(torch.utils.data.Dataset):
             max_length=self.max_length
         )
 
+        # Pad or truncate label to max_length to match the sequence
+        label = label[:self.max_length]  # truncate if too long
+        pad_length = self.max_length - label.size(0)
+        if pad_length > 0:
+            label = torch.nn.functional.pad(label, (0, pad_length), value=0)
+
         return {
             "input_ids": tokenized_seq["input_ids"].squeeze(0),
             "attention_mask": tokenized_seq["attention_mask"].squeeze(0),
             "label": label
+        }
+
+
+class PrecomputedUniprotDataset(torch.utils.data.Dataset):
+    """
+    Dataset that loads precomputed ESM-2 embeddings from memory-mapped files for the Uniprot dataset.
+    Run precompute_embeddings.py first to generate the required files.
+    """
+    def __init__(self, fold=None, root="./data/train_val/precomputed_embeddings"):
+        super().__init__()
+        metadata = torch.load(os.path.join(root, "metadata.pt"), weights_only=False)
+        self._total = metadata["length"]
+        self._max_length = metadata["max_length"]
+        self._hidden_dim = metadata["hidden_dim"]
+        self._root = root
+
+        all_folds = metadata["fold"]  # list of ints
+        all_labels = metadata["labels"]  # tensor of shape (total, max_length)
+
+        if fold is not None:
+            mask = np.isin(all_folds, fold)
+            self.indices = np.where(mask)[0]
+        else:
+            self.indices = np.arange(self._total)
+
+        self.labels = all_labels[self.indices]
+
+        self._embeddings = None
+        self._masks = None
+
+    def open_memmaps(self):
+        self._embeddings = np.memmap(
+            os.path.join(self._root, "embeddings.dat"),
+            dtype=np.float16, mode="r", shape=(self._total, self._max_length, self._hidden_dim)
+        )
+        self._masks = np.memmap(
+            os.path.join(self._root, "masks.dat"),
+            dtype=np.bool_, mode="r", shape=(self._total, self._max_length)
+        )
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx):
+        if self._embeddings is None:
+            self.open_memmaps()
+        real_idx = self.indices[idx]
+        return {
+            "embedding": torch.from_numpy(self._embeddings[real_idx].copy()),
+            "attention_mask": torch.from_numpy(self._masks[real_idx].copy()).long(),
+            "label": self.labels[idx].float(),
         }
